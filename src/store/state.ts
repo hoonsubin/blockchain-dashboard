8f87b83efda7c72af09b09a853c4c1edb2ad1b50
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2021-01-20 17:38:02 +0900

    updated packages

--- src/store/state.ts
+++ src/store/state.ts
@@ -1,10 +1,8 @@
 import Web3 from 'web3';
-import { ApiPromise } from '@polkadot/api';
 
 export const state = {
     counter: 0,
     web3Inst: Web3,
-    substrateInst: ApiPromise,
 };
 
 export type State = typeof state;
